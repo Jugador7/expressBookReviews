114395121b21455dfa9aa89c58aad97e80441f51
Jugador7 <moyagabriel14@gmail.com>
2024-05-14 12:20:26 -0500

    finished the easy part boss

--- final_project/index.js
+++ final_project/index.js
@@ -8,7 +8,11 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use(session({
+    secret:"fingerprint_customer",
+    resave: true, 
+    saveUninitialized: true}
+))
 
 app.use("/customer/auth/*", function auth(req,res,next){
     if(req.session.authorization) {
@@ -32,4 +36,12 @@ const PORT =5000;
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
+// After all route handlers
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
 app.listen(PORT,()=>console.log("Server is running"));
+
+
--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const session = require('express-session')
 const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
@@ -6,12 +7,6 @@ const regd_users = express.Router();
 let users = [];
 
 const app = express();
-
-app.use(session({
-  secret: 'your_secret_key',
-  resave: false,
-  saveUninitialized: true
-}));
 
 const isValid = (username)=>{ //returns boolean
   let userswithsamename = users.filter((user)=>{
@@ -38,8 +33,8 @@ const authenticatedUser = (username,password)=>{ //returns boolean
 //only registered users can login
 
 regd_users.post("/login", (req,res) => {
-  const username = req.body.username;
-  const password = req.body.password;
+  console.log("Login request received:", req.body);
+  const { username, password } = req.body;
 
   if (!username || !password) {
       return res.status(404).json({message: "Error logging in"});
@@ -62,9 +57,31 @@ regd_users.post("/login", (req,res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  let book = books[isbn]
+  if (book) { //Check is friend exists
+      let review = req.body.review;
+      if(review) {
+        book.reviews.push(review);  // Push new review into the reviews array
+        res.status(200).send(`Review added to book with ISBN ${isbn}.`);
+      } else {
+        res.status(400).send("No review provided!");
+      }
+  }
+  else{
+      res.status(404).send("Unable to find book!");
+  }
 });
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const isbn = req.params.isbn;
+  if (isbn){
+      delete books[isbn]
+  }
+  res.send(`Review with the isbn  ${isbn} deleted.`);
+});
+
+
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
